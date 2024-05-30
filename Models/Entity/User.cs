c96539ca0de47d1844e6d8da5cc70466e8b8aa00
TuanTran <tuan.tranvan3012@gmail.com>
2024-05-30 14:47:11 +0700

    Add User model

---
+++ Models/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoIdentityProject.Models.Entity
+{
+    public class User : IdentityUser
+    {
+        [StringLength(100)]
+        [MaxLength(100)]
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
