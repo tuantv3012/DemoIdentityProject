db255a84612871d881233ba35c72f227e8ab1360
TuanTran <tuan.tranvan3012@gmail.com>
2024-05-30 14:48:33 +0700

    Add nuget and migrations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,13 @@
 using DemoIdentityProject.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace DemoIdentityProject.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
